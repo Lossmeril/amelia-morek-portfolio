75a34907f91e88646cc85db6dcfe9385acbdb14d
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2023-09-30 15:10:28 +0200

    style: responsive

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,20 @@ export default function Home() {
     /*  MADE WITH MINIMALISM, REACT AND LOVE  */
     <main className="">
       <div className="w-full h-full min-h-screen grid grid-rows-4 grid-cols-1 md:grid-rows-2 md:grid-cols-2">
-        <div className="w-full h-full relative p-[10%] outline-black outline outline-1">
-          <div className="">
+        <div className="w-full h-full relative p-12 xl:p-[10%] xl:pl-[15%] xl:pt-[7%] outline-black outline outline-1">
+          <div className="mt-[50%] md:mt-0">
             <div className="z-20">
               <p className="text-xl">Hi, I am </p>
               <h1 className="font-bold text-4xl">Amelia Roża Morek</h1>
             </div>
-            <Image
-              width={350}
-              height={350}
-              alt="Amelia Morek"
-              src="/img/amelia.jpg"
-              className="absolute translate-x-[80%] -translate-y-20 -z-10"
-            />
+            <div className="w-[200px] h-[200px] md:w-[250px] md:h-[250px] xl:w-[350px] xl:h-[350px] absolute translate-x-[50%] xl:translate-x-[80%] translate-y-[65%] md:translate-y-10 xl:-translate-y-20 -z-10">
+              <Image
+                fill
+                alt="Amelia Morek"
+                src="/img/amelia.jpg"
+                className="object-cover"
+              />
+            </div>
             <div className="z-20 mt-12">
               <p className="text-xl italic">mixed media artist</p>
               <p className="text-xl italic">filmmaker</p>
@@ -33,6 +34,45 @@ export default function Home() {
             <li className="mb-5">writing</li>
           </ul>
         </div>
+        <div className="w-full h-screen md:h-full relative outline-black outline outline-1">
+          <div className="w-full h-[15%] relative">
+            <h2 className="italics text-center text-2xl md:text-3xl lowercase font-bold centered-absolute mt-1">
+              Featured projects:
+            </h2>
+          </div>
+          <div className="w-full h-[85%] grid grid-rows-2 md:grid-rows-1 grid-cols-1 md:grid-cols-2">
+            <div
+              className="overflow-hidden w-[75%] aspect-square md:aspect-auto mx-auto md:my-5 relative"
+              style={{
+                backgroundImage: "url(/img/lrad.jpg)",
+                backgroundSize: "cover",
+              }}
+            >
+              <div className="centered-absolute z-10 text-white text-center">
+                <h3 className="font-bold text-xl lowercase italic">
+                  Like Real Adults do
+                </h3>
+                <p className="italic text-md">short film</p>
+                <p className="italic text-sm">2023</p>
+              </div>
+            </div>
+            <div
+              className="overflow-hidden w-[75%] aspect-square md:aspect-auto mx-auto md:my-5 relative"
+              style={{
+                backgroundImage: "url(/img/lrad.jpg)",
+                backgroundSize: "cover",
+              }}
+            >
+              <div className="centered-absolute z-10 text-white text-center">
+                <h3 className="font-bold text-xl lowercase italic">
+                  Like Real Adults do
+                </h3>
+                <p className="italic text-md">short film</p>
+                <p className="italic text-sm">2023</p>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="w-full h-full relative p-[10%] outline-black outline outline-1">
           <div className="centered-absolute">
             <h2 className="italics text-center text-3xl lowercase font-bold mb-8">
@@ -46,29 +86,6 @@ export default function Home() {
               <BsInstagram size={25} />
               <BsLinkedin size={25} />
             </div>
-          </div>
-        </div>
-        <div className="w-full h-full relative outline-black outline outline-1">
-          <div className="h-[15%] relative">
-            <h2 className="italics text-center text-3xl lowercase font-bold centered-absolute">
-              Featured projects:
-            </h2>
-          </div>
-          <div className="w-full h-[85%] grid grid-rows-1 grid-cols-2">
-            <div
-              className="overflow-hidden ml-10 my-5 mr-3"
-              style={{
-                backgroundImage: "url(/img/lrad.jpg)",
-                backgroundSize: "cover",
-              }}
-            ></div>
-            <div
-              className="overflow-hidden mr-10 my-5 ml-3"
-              style={{
-                backgroundImage: "url(/img/lrad.jpg)",
-                backgroundSize: "cover",
-              }}
-            ></div>
           </div>
         </div>
       </div>
